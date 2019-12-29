8211a6c00b17e2acdbf17d0904aeee099ec60956
Tomek <tomlucki@wp.pl>
2019-12-29 18:47:06 +0100

    [ToDo-app] Added sorting

--- src/main/java/pl/lucky/controller/TaskController.java
+++ src/main/java/pl/lucky/controller/TaskController.java
@@ -2,26 +2,29 @@ package pl.lucky.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.lucky.model.Task;
 import pl.lucky.services.TaskService;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class TaskController {
 
     private TaskService taskService;
-    private int globalCurrentPage;
-    private int globalPageSize;
+    private Pageable pageable;
 
     @Autowired
     public TaskController(TaskService taskService) {
@@ -30,73 +33,55 @@ public class TaskController {
 
     @PostMapping("/")
     public String saveTask(@Valid @ModelAttribute Task task, BindingResult result, Model model,
-                           @RequestParam("page") Optional<Integer> page,
-                           @RequestParam("size") Optional<Integer> size) {
+                           RedirectAttributes redirectAttributes,
+                           @PageableDefault(size = 5, sort = "taskCreateTime", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
 
         if (result.hasErrors()) {
-            getPaginatedTasks(model, page, size);
+            getPaginatedTasks(model, this.pageable != null ? this.pageable : pageable);
             return "index";
         }
         taskService.save(task);
+        addRedirectAttributes(redirectAttributes, pageable);
         return "redirect:/";
     }
 
     @GetMapping("/")
-    public String showAllTasks(Model model,
-                               @RequestParam("page") Optional<Integer> page,
-                               @RequestParam("size") Optional<Integer> size) {
+    public String showAllTasks(Model model, @PageableDefault(size = 5, sort = "taskCreateTime", direction = Sort.Direction.DESC) Pageable pageable) {
+        this.pageable = pageable;
         Task task = new Task();
-        model.addAttribute(task);
-        getPaginatedTasks(model, page, size);
+        model.addAttribute("task", task);
+        getPaginatedTasks(model, pageable);
         return "index";
     }
 
-    private void getPaginatedTasks(Model model, Optional<Integer> page, Optional<Integer> size) {
-        int currentPage = getCurrentPage(page);
-        int pageSize = getPageSize(size);
+    private void getPaginatedTasks(Model model, Pageable pageable) {
+        Page<Task> taskPage = taskService.findAllPaginated(pageable);
 
-        Page<Task> taskPage = taskService.findAllPaginated(PageRequest.of(currentPage - 1, pageSize));
+
+        List<Sort.Order> sortOrders = taskPage.getSort().stream().collect(Collectors.toList());
+        if (sortOrders.size() > 0) {
+            Sort.Order order = sortOrders.get(0);
+            model.addAttribute("sortProperty", order.getProperty());
+            model.addAttribute("sortDesc", order.getDirection() == Sort.Direction.DESC);
+        }
         model.addAttribute("taskPage", taskPage);
-
-        int totalPages = taskPage.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                    .boxed()
-                    .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-    }
-
-    private int getPageSize(Optional<Integer> size) {
-        int pageSize;
-        if (size.isPresent()) {
-            pageSize = size.orElse(5);
-            this.globalPageSize = pageSize;
-        } else if (this.globalPageSize != 0) {
-            pageSize = this.globalPageSize;
-        } else {
-            pageSize = 5;
-        }
-        return pageSize;
-    }
-
-    private int getCurrentPage(Optional<Integer> page) {
-        int currentPage;
-        if (page.isPresent()) {
-            currentPage = page.orElse(1);
-            this.globalCurrentPage = currentPage;
-        } else if (this.globalCurrentPage != 0) {
-            currentPage = this.globalCurrentPage;
-        } else {
-            currentPage = 1;
-        }
-        return currentPage;
     }
 
     @PostMapping("/delete/{id}")
-    public String deleteTask(@PathVariable Long id) {
+    public String deleteTask(@PathVariable Long id,
+                             RedirectAttributes redirectAttributes,
+                             @PageableDefault(size = 5, sort = "taskCreateTime", direction = Sort.Direction.DESC) Pageable pageable) {
         taskService.delete(id);
+        addRedirectAttributes(redirectAttributes, pageable);
         return "redirect:/";
+    }
+
+    private RedirectAttributes addRedirectAttributes(RedirectAttributes redirectAttributes, Pageable pageable) {
+        redirectAttributes.addAttribute("page", this.pageable != null ? this.pageable.getPageNumber() : pageable.getPageNumber());
+        redirectAttributes.addAttribute("size", this.pageable != null ? this.pageable.getPageSize() : pageable.getPageSize());
+        redirectAttributes.addAttribute("sort", this.pageable != null ? this.pageable.getSort().toString().replaceFirst(": ", ",") : pageable.getSort().toString().replaceFirst(": ", ","));
+        return redirectAttributes;
     }
 
 }
--- src/main/java/pl/lucky/repository/TaskRepository.java
+++ src/main/java/pl/lucky/repository/TaskRepository.java
@@ -1,5 +1,7 @@
 package pl.lucky.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +14,7 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> findByUser_IdOrderByTaskCreateTimeDesc(Long ownerId);
-
-    List<Task> findAllByOrderByTaskCreateTimeDesc();
+    Page<Task> findByUser_Id(Long ownerId, Pageable pageable);
 
     @Query("SELECT t FROM tasks t WHERE t.taskDeadline = :tomorrow")
     List<Task> findAllByTaskDeadline(@Param("tomorrow") LocalDate tomorrow);
